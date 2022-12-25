9c517d42684970313ae163036feeb8e845415bd5
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2022-12-25 21:46:36 +0800

    updated

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink
+} from '@apollo/client';
+
+const createApolloClient = () =>{
+  const link = new HttpLink({
+    uri: '/api/graphql'
+  });
+
+  return new ApolloClient(
+    {
+      link,
+      cache: new InMemoryCache()
+    }
+  );
+};
+
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={createApolloClient()}>
+  <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
