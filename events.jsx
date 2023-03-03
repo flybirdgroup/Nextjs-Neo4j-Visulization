03d4cebbc0c4de5d0089b954e9fc5e17ac620392
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2023-03-03 09:22:03 +0800

    Update events.jsx

--- events.jsx
+++ events.jsx
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
+import { useState } from 'react'
 
 const Home = () => {
+  const [activeTab, setActiveTab] = useState('networks')
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab)
+  }
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <Head>
@@ -16,28 +22,40 @@ const Home = () => {
           <div>
             <Image src="/logo.png" alt="Logo" width={120} height={40} />
           </div>
-          <ul className="flex">
-            <li className="mr-6">
-              <Link href="/">
-                <a className="text-gray-600 hover:text-gray-800 font-medium">My Networks</a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/events">
-                <a className="text-gray-600 hover:text-gray-800 font-medium">Events</a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/find-people">
-                <a className="text-gray-600 hover:text-gray-800 font-medium">Find People</a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/groups">
-                <a className="text-gray-600 hover:text-gray-800 font-medium">Groups</a>
-              </Link>
-            </li>
-          </ul>
+          <div className="flex">
+            <button
+              className={`text-gray-600 hover:text-gray-800 font-medium mr-6 ${
+                activeTab === 'networks' && 'text-blue-500'
+              }`}
+              onClick={() => handleTabClick('networks')}
+            >
+              My Networks
+            </button>
+            <button
+              className={`text-gray-600 hover:text-gray-800 font-medium mr-6 ${
+                activeTab === 'events' && 'text-blue-500'
+              }`}
+              onClick={() => handleTabClick('events')}
+            >
+              Events
+            </button>
+            <button
+              className={`text-gray-600 hover:text-gray-800 font-medium mr-6 ${
+                activeTab === 'find-people' && 'text-blue-500'
+              }`}
+              onClick={() => handleTabClick('find-people')}
+            >
+              Find People
+            </button>
+            <button
+              className={`text-gray-600 hover:text-gray-800 font-medium mr-6 ${
+                activeTab === 'groups' && 'text-blue-500'
+              }`}
+              onClick={() => handleTabClick('groups')}
+            >
+              Groups
+            </button>
+          </div>
           <div className="flex items-center">
             <input className="bg-gray-200 rounded-l-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="search" type="text" placeholder="Search..." />
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-full focus:outline-none focus:shadow-outline" type="button">
@@ -48,24 +66,16 @@ const Home = () => {
       </header>
 
       <main className="container mx-auto px-4 py-6">
-        <h1 className="text-2xl font-medium mb-4">Events</h1>
-        <div className="flex flex-wrap -mx-4">
-          <div className="w-full lg:w-1/3 px-4 mb-8">
-            <div className="bg-white rounded-lg shadow-md">
-              <Image src="/event-image-1.jpg" alt="Event" width={400} height={225} className="rounded-t-lg" />
-              <div className="p-4">
-                <h2 className="text-xl font-medium mb-2">Event Title</h2>
-                <p className="text-gray-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <div className="flex items-center justify-between">
-                  <p className="text-gray-600">Date: March 15, 2023</p>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" type="button">
-                    Register
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-full lg:w-1/3 px
+        <h1 className="text-2xl font-medium mb-4">{activeTab === 'events' ? 'Events' : activeTab === 'find-people' ? 'Find People' : activeTab === 'groups' ? 'Groups' : 'My Networks'}</h1>
+        {activeTab === 'events' && (
+          <div className="flex flex-wrap -mx-4">
+            <div className="w-full lg:w-1/3 px-4 mb-8">
+              <div className="bg-white rounded-lg shadow-md">
+                <Image src="/event-image-1.jpg" alt="Event" width={400} height={225} className="rounded-t-lg" />
+                <div className="p-4">
+                  <h2 className="text-xl font-medium mb-2">Event Title</h2>
+                  <p className="text-gray-600 mb-4">Lorem ipsum dolor sit amet
+
 
 
 
